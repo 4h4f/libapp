4c814fb0985aedffd718577e69512cc178f7e146
abdelrhman <abdelrhman-mohammed@hotmail.com>
2022-10-23 18:33:21 +0300

    Create first_screen.dart
    fist screen added

---
+++ lib/screens/first_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('hello abdo'),
+        ),
+      ),
+    );
+  }
+}
